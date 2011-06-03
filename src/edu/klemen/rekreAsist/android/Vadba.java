49fefb993e2b5bfbceaacded96ccce968d2f08ff
KlemenK <klemen.kodric@gmail.com>
2011-06-03 15:52:50 +0200

    polje podatkov- kasneje za bazo

---
+++ src/edu/klemen/rekreAsist/android/Vadba.java
@@ -0,0 +1,23 @@
+package edu.klemen.rekreAsist.android;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.location.Location;
+
+public class Vadba {
+	public ArrayList<Location> pot;
+	public List<Float> hitrost;
+	public int stKrogov;
+	public float maxSpeed;
+//	public String time;
+	
+	public Vadba(ArrayList<Location> p, List<Float> h, int stK, float max/*, String t*/){
+		pot=p;
+		hitrost=h;
+		stKrogov=stK;
+		maxSpeed=max;
+		//time=t;
+	}
+
+}
